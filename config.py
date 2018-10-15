66e99bec412a2f2411c221fd5487cce46d8b5982
Steven Abadie <steven@anothernewthing.com>
2018-10-14 18:24:53 -0600

    Remove flask-admin

--- config.py
+++ config.py
@@ -9,5 +9,3 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECURITY_PASSWORD_HASH = 'sha512_crypt'
-    SECURITY_PASSWORD_SALT = os.environ.get('PASSWORD_SALT')
--- volunteermatching/__init__.py
+++ volunteermatching/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_security import Security, SQLAlchemyUserDatastore
-from flask_security.utils import encrypt_password
 from config import Config
 
 app = Flask(__name__)
@@ -12,10 +10,6 @@ migrate = Migrate(app, db)
 
 #from volunteermatching import views
 from volunteermatching.models import Role, User
-
-# Setup Flask-Security
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
 
 if __name__ == '__main__':
     app.run()
