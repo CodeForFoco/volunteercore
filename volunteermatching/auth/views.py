09c5271effcc00c6e60325ef22ba9b5676591f32
Steven Abadie <steven@anothernewthing.com>
2018-10-17 20:14:29 -0600

    Add create_user view and template

--- volunteermatching/auth/views.py
+++ volunteermatching/auth/views.py
@@ -1,8 +1,8 @@
-from volunteermatching import app
+from volunteermatching import app, db
 from .models import User
-from .forms import LoginForm
+from .forms import LoginForm, CreateUser
 from flask import render_template, request, flash, url_for, redirect
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
 @app.route('/login', methods=["GET", "POST"])
@@ -26,3 +26,15 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/admin/create_user', methods=["GET", "POST"])
+@login_required
+def create_user():
+    form = CreateUser()
+    if form.validate_on_submit():
+        user = User(email=form.email.data)
+        user.hash_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('auth/create_user.html', title='Create User', form=form)
