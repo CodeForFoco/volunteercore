0f792acef2555418d84d9400b14389695969c1d6
Steven Abadie <steven@anothernewthing.com>
2018-10-17 20:16:18 -0600

    Add template_folder to auth blueprint

--- volunteermatching/auth/__init__.py
+++ volunteermatching/auth/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-bp_auth = Blueprint('auth', __name__)
+bp_auth = Blueprint('auth', __name__, template_folder="templates")
 
 from .models import Role, User
 from . import views, forms
