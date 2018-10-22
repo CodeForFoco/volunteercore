4565af866fd3f83c09a7e0663306bf9a9ea0bba3
Steven Abadie <steven@anothernewthing.com>
2018-10-21 20:56:06 -0600

    Add Skill model and Opportunity many to many relationship

--- volunteermatching/volops/models.py
+++ volunteermatching/volops/models.py
@@ -10,6 +10,10 @@ age_group_interests = db.Table('age_group_interests',
     db.Column('age_group_interests_id', db.Integer, db.ForeignKey('age_group_interest.id'))
 )
 
+skills = db.Table('skills',
+    db.Column('opportunity_id', db.Integer, db.ForeignKey('opportunity.id')),
+    db.Column('skills_id', db.Integer, db.ForeignKey('skill.id'))
+)
 
 class Partner(db.Model):
     id = db.Column(db.Integer(), primary_key=True, index=True)
@@ -32,11 +36,17 @@ class Opportunity(db.Model):
     end_date = db.Column(db.Date())
     training_time_required = db.Column(db.Integer())
     volunteers_needed = db.Column(db.Integer())
+
+    # One to many relationships
     partner_id = db.Column(db.Integer, db.ForeignKey('partner.id'))
+
+    # Many to many relations
     passions = db.relationship('Passion', secondary='passions',
         lazy='subquery', backref=db.backref('opportunities', lazy=True))
     age_group_interests = db.relationship('AgeGroupInterest',
         secondary='age_group_interests', lazy='subquery',
+        backref=db.backref('opportunities', lazy=True))
+    skills = db.relationship('Skill', secondary='skills', lazy='subquery',
         backref=db.backref('opportunities', lazy=True))
 
     def __repr__(self):
@@ -56,3 +66,11 @@ class AgeGroupInterest(db.Model):
 
     def __repr__(self):
         return '<AgeGroupInterest {}>'.format(self.name)
+
+
+class Skill(db.Model):
+    id = db.Column(db.Integer(), primary_key=True, index=True)
+    name = db.Column(db.String(50))
+
+    def __repr__(self):
+        return '<Skill {}>'.format(self.name)
