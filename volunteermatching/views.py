58905a8d97e2862b103e04a81c87dd516e00db01
Steven Abadie <steven@anothernewthing.com>
2018-10-22 21:34:53 -0600

    Add base admin view and template

--- volunteermatching/views.py
+++ volunteermatching/views.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from volunteermatching import app
 from flask_login import login_required
+from volunteermatching.auth.views import users
 
 
 @app.route('/')
@@ -8,3 +9,8 @@ from flask_login import login_required
 @login_required
 def index():
     return render_template("index.html", title="Home Page")
+
+@app.route('/admin/')
+@login_required
+def admin():
+    return redirect(url_for('users'))
