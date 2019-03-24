c74d676ace1f65cf7462b0620eebf3c7cac31e38
Steven Abadie <steven@anothernewthing.com>
2019-03-24 16:03:48 -0600

    Restructure auth blueprint

--- volunteermatching/__init__.py
+++ volunteermatching/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from config import Config
 
@@ -8,11 +9,12 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
 
 
 # Register blueprints
-from .auth import bp_auth
-app.register_blueprint(bp_auth)
+from volunteermatching.auth import bp as auth_bp
+app.register_blueprint(auth_bp)
 from .volops import bp_volops
 app.register_blueprint(bp_volops)
 from volunteermatching.api import bp as api_bp
