d934605affa3416458423f490bd9cc290a58958c
Steven Abadie <steven@anothernewthing.com>
2019-04-24 22:12:13 -0600

    Add DATABASE_URL to config for Heroku use

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = "postgresql://" + \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        "postgresql://" + \
         os.environ.get('DATABASE_USER') + ":" + \
         os.environ.get('DATABASE_PASSWORD') + "@localhost/" + \
         os.environ.get('DATABASE_NAME') or \
