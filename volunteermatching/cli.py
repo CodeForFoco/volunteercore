3297ffaa3f45e6402fe5ad3b8eef218f0e9c3cd9
Steven Abadie <steven@anothernewthing.com>
2019-04-26 21:44:15 -0600

    Add auto admin user and whoosh index commands

--- volunteermatching/cli.py
+++ volunteermatching/cli.py
@@ -1,6 +1,7 @@
 import click
 from volunteermatching import db
 from volunteermatching.auth.models import User
+from flask_whooshalchemyplus import index_all
 
 
 def register(app):
@@ -17,3 +18,20 @@ def register(app):
             click.echo('Admin user created')
         else:
             click.echo('Admin user already exists')
+
+    @app.cli.command()
+    def auto_create_admin():
+        """Automatically create default admin user"""
+        if not User.query.filter_by(username='admin').first():
+            admin = User(username='admin')
+            admin.hash_password('password')
+            db.session.add(admin)
+            db.session.commit()
+            click.echo('Default admin user created')
+        else:
+            click.echo('Admin user already exists')
+
+    @app.cli.command()
+    def whoosh_index_all():
+        """Index searcheable models for Whoosh """
+        index_all(app)
