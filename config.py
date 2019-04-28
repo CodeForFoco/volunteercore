2f66c743ed763f1e8d18a277f74ae49cebc4b2bc
Steven Abadie <steven@anothernewthing.com>
2019-04-27 20:57:27 -0600

    Correct database config to allow postgres or sqlite

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
+if os.environ.get('DATABASE_SYSTEM') == "postgres":
+    os.environ["DATABASE_URL"] = "postgresql://" + \
+    os.environ.get('DATABASE_USER') + ":" + \
+    os.environ.get('DATABASE_PASSWORD') + "@localhost/" + \
+    os.environ.get('DATABASE_NAME')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        "postgresql://" + \
-        os.environ.get('DATABASE_USER') + ":" + \
-        os.environ.get('DATABASE_PASSWORD') + "@localhost/" + \
-        os.environ.get('DATABASE_NAME') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WHOOSH_BASE = basedir + '/whoosh/'
     JWT_TOKEN_LOCATION = ('cookies', 'headers')
