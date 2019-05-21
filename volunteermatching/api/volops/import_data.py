414acde1d4c96c6a2ebcb63ce16de60b0967c671
Steven Abadie <steven@anothernewthing.com>
2019-05-20 21:19:32 -0600

    Add opportunity tags import and make import column names better match model field names
    #20

--- volunteermatching/api/volops/import_data.py
+++ volunteermatching/api/volops/import_data.py
@@ -32,18 +32,19 @@ def import_tags():
             file_read = csv.DictReader(open_csv, delimiter=',')
             items_imported = 0
             for row in file_read:
-                tag = Tag(name=row['Descript'])
-                if not TagCategory.query.filter_by(name=row['Type']).first():
-                    tag_category = TagCategory(name=row['Type'])
+                tag = Tag(name=row['tag'])
+                if not TagCategory.query.filter_by(
+                        name=row['tag_category']).first():
+                    tag_category = TagCategory(name=row['tag_category'])
                     db.session.add(tag_category)
                     db.session.commit()
                 if Tag.query.filter_by(
-                        name=row['Descript'],
+                        name=row['tag'],
                         tag_category_id=TagCategory.query.filter_by(
-                        name=row['Type']).first().id).first():
+                        name=row['tag_category']).first().id).first():
                     continue
                 tag.tag_category_id = TagCategory.query.filter_by(
-                    name=row['Type']).first().id
+                    name=row['tag_category']).first().id
                 db.session.add(tag)
                 db.session.commit()
                 items_imported += 1
@@ -52,7 +53,7 @@ def import_tags():
         return str(items_imported) + ' tags imported'
 
 
-# API POST endpoint for importing Tag data
+# API POST endpoint for importing Opportunity data
 @bp.route('/api/import/opportunities', methods=['POST'])
 @token_auth.login_required
 @requires_roles('Admin')
@@ -71,33 +72,33 @@ def import_opportunities():
             items_imported = 0
             for row in file_read:
                 if Opportunity.query.filter_by(
-                        name=row['Jobname']).first():
+                        name=row['opportunity']).first():
                     continue
                 opportunity = Opportunity(
-                        name=row['Jobname'],
-                        location_street=row['Address1'],
-                        location_city=row['City'],
-                        location_state=row['State'],
-                        location_zip=row['Zip'],
-                        volunteers_needed=row['Requested']
+                        name=row['opportunity'],
+                        location_street=row['location_street1'],
+                        location_city=row['location_city'],
+                        location_state=row['location_state'],
+                        location_zip=row['location_zip'],
+                        volunteers_needed=row['volunteers_needed']
                     )
-                if row['Reqdate']:
+                if row['start_date']:
                     opportunity.start_date = datetime.strptime(
-                        row['Reqdate'], '%Y%m%d').date()
-                if row['Enddate']:
+                        row['start_date'], '%Y%m%d').date()
+                if row['end_date']:
                     opportunity.end_date = datetime.strptime(
-                        row['Enddate'], '%Y%m%d').date()
-                if row['Address2']:
-                    Opportunity.location_street = row['Address1'] \
-                        + ', ' + row['Address2']
+                        row['end_date'], '%Y%m%d').date()
+                if row['location_street2']:
+                    Opportunity.location_street = row['location_street1'] \
+                        + ', ' + row['location_street2']
                 if not Partner.query.filter_by(
-                        name=row['Sitename']).first():
-                    partner = Partner(name=row['Sitename'])
+                        name=row['partner']).first():
+                    partner = Partner(name=row['partner'])
                     db.session.add(partner)
                     db.session.commit()
                     index_one_record(partner)
                 opportunity.partner_id = Partner.query.filter_by(
-                    name=row['Sitename']).first().id
+                    name=row['partner']).first().id
                 db.session.add(opportunity)
                 db.session.commit()
                 index_one_record(opportunity)
@@ -105,3 +106,40 @@ def import_opportunities():
         open_csv.close()
         os.remove(os.path.join(Config.UPLOAD_FOLDER, import_filename))
         return str(items_imported) + ' Opportunities imported'
+
+
+# API POST endpoint for importing Opportunity Tags data
+@bp.route('/api/import/opportunity_tags', methods=['POST'])
+@token_auth.login_required
+@requires_roles('Admin')
+def import_opportunity_tags():
+    if 'file' not in request.files:
+        return bad_request('must provide a file')
+    import_csv = request.files['file']
+    if import_csv.filename == '':
+        return bad_request('must provide a file')
+    if import_csv and import_csv.filename.rsplit('.', 1)[1].lower() == 'csv':
+        import_filename = secure_filename(import_csv.filename)
+        import_csv.save(os.path.join(Config.UPLOAD_FOLDER, import_filename))
+        with open(os.path.join(
+                Config.UPLOAD_FOLDER, import_filename), 'r') as open_csv:
+            file_read = csv.DictReader(open_csv, delimiter=',')
+            items_imported = 0
+            for row in file_read:
+                if not Opportunity.query.filter_by(
+                        name=row['opportunity']).first() or not \
+                        row['tag'] or not row['tag_category']:
+                    continue
+                tag_category_id = TagCategory.query.filter_by(
+                    name=row['tag_category']).first().id
+                opportunity = Opportunity.query.filter_by(
+                    name=row['opportunity']).first()
+                opportunity.tags.append(Tag.query.filter_by(
+                    tag_category_id=tag_category_id,
+                    name=row['tag']).first())
+                db.session.add(opportunity)
+                db.session.commit()
+                items_imported += 1
+        open_csv.close()
+        os.remove(os.path.join(Config.UPLOAD_FOLDER, import_filename))
+        return str(items_imported) + ' opportunity tags imported'
