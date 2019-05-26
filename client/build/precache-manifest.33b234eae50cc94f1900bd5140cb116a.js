08f3eb9f16b00350af2eb23af98732253ce4320a
Steven Abadie <steven@anothernewthing.com>
2019-05-26 14:55:40 -0600

    Add pre-built front end for use in production

---
+++ client/build/precache-manifest.33b234eae50cc94f1900bd5140cb116a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "916942c196ad0c94ffa3",
+    "url": "/static/js/main.72deb473.chunk.js"
+  },
+  {
+    "revision": "09b05372cd0cebe052ec",
+    "url": "/static/js/2.bb774fb1.chunk.js"
+  },
+  {
+    "revision": "916942c196ad0c94ffa3",
+    "url": "/static/css/main.d12df9a6.chunk.css"
+  },
+  {
+    "revision": "afb0be6c6f63ffaa2c77bc08869821fc",
+    "url": "/index.html"
+  }
+];
