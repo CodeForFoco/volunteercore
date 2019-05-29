110be9dab76c7fd0a17fc7a26055eee5f580b34a
Alex-Cannon <alexcannon2018@gmail.com>
2019-05-29 11:57:06 -0600

    bug fix: tags can now be added.

--- client/src/objects/SelectMany/SelectMany.js
+++ client/src/objects/SelectMany/SelectMany.js
@@ -37,8 +37,8 @@ export default class SelectMany extends Component {
   }
 
   addOption(option) {
-    let arr = this.props.value;
-    if (!arr || arr.indexOf(option) !== -1) return;
+    let arr = this.props.value || [];
+    if (arr.indexOf(option) !== -1) return;
     arr.push(option);
     this.props.setValue({ [this.props.name]: arr });
   }
@@ -53,9 +53,6 @@ export default class SelectMany extends Component {
     this._isMounted = true;
     if (!this.props.options && (this.props.getOptions || this.props.customGet)) {
       this.getOptions();
-    }
-    if (!this.props.value || !Array.isArray(this.props.value)) {
-      this.props.setValue({ [this.props.name]: [] });
     }
   }
 
