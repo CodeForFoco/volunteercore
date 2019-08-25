04a6b44e083e285e7ccaa68513bf2d704cc5be0a
Steven Abadie <steven@anothernewthing.com>
2019-08-25 08:13:07 -0600

    Update frontend build for heroku release

---
+++ client/build/precache-manifest.932f9f78f840e8b60a1a03d3f7af7730.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ebf32d52b67e3e854291",
+    "url": "/static/js/main.6ef09ee4.chunk.js"
+  },
+  {
+    "revision": "8eff7a855a2b2f0d47e6",
+    "url": "/static/js/2.e488d505.chunk.js"
+  },
+  {
+    "revision": "ebf32d52b67e3e854291",
+    "url": "/static/css/main.a5d0435b.chunk.css"
+  },
+  {
+    "revision": "f6b7b286849b5657e1a10d4173602a8e",
+    "url": "/index.html"
+  }
+];
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aea9f23985fde06b0266ca7598090029.js"
+  "/precache-manifest.932f9f78f840e8b60a1a03d3f7af7730.js"
 );
 
 workbox.clientsClaim();
